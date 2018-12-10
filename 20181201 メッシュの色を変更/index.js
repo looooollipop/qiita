9256c03b7231bff8a42b8a0d4e98d1040fdd3546
looooollipop <45483511+looooollipop@users.noreply.github.com>
2018-12-10 13:30:40 +0900

    Update index.js

--- 20181201 メッシュの色を変更/index.js
+++ 20181201 メッシュの色を変更/index.js
@@ -23,7 +23,7 @@ function init(){
     camera.lookAt(new THREE.Vector3(0,0,0)); //原点方向を向く ※OrbitControl.jsを使用しているときは無効
 
     //メッシュを作成
-    var geometry=new THREE.BoxGeometry(400, 400, 400);
+    var geometry=new THREE.BoxGeometry(400,400,400);
     var material=new THREE.MeshStandardMaterial({color:"#00ffff"}); //※StandardMaterialの使用時はライトを準備
     var mesh=new THREE.Mesh(geometry, material);
     scene.add(mesh);
